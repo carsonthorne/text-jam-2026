06c51ab446d994790a90a394eafe246b854d0a58
Carson <carson.thorne@hotmail.com>
2026-05-22 15:03:48 -0230

    Messing with the colors some more

--- board_definition.py
+++ board_definition.py
@@ -192,31 +192,66 @@ ZONE_CURSOR_STARTS = {
     "NW": (-7,5)
 }
 
+            # zone                 # piece
+RED =    ["rgb(180, 15, 25)",  "rgb(255, 30, 60)"]
+PURPLE = ["rgb(135, 10, 185)", "rgb(190, 0, 250)"]
+GREEN =  ["rgb(10, 155, 10)",    "rgb(5, 200, 5)"]
+ORANGE = ["rgb(235, 95, 0)",  "rgb(255, 155, 0)"]
+
 COLORS = {
+    
     "N": {
-        "zone": "red1",
-        "piece": "red3"
+        "zone": RED[0],
+        "piece": RED[1]
     },
     "NE": {
-        "zone": "white",
-        "piece": "bright_black"
+        "zone": "bright_black",
+        "piece": "white"
     },
     "SE": {
-        "zone": "purple4",
-        "piece": "blue_violet"
+        "zone": PURPLE[0],
+        "piece": PURPLE[1]
     },
     "S": {
-        "zone": "spring_green3",
-        "piece": "spring_green4"
+        "zone": GREEN[0],
+        "piece": GREEN[1]
     },
     "SW": {
         "zone": "dodger_blue2",
-        "piece": "dark_blue"
+        "piece": "dodger_blue1"
     },
     "NW": {
-        "zone": "dark_orange",
-        "piece": "orange_red1"
+        "zone": ORANGE[0],
+        "piece": ORANGE[1]
     }
+
+
+
+    # "N": {
+    #     "zone": RED[0],
+    #     "piece": GREEN[1]
+    # },
+    # "NE": {
+    #     "zone": "bright_black",
+    #     "piece": "dodger_blue1"
+    # },
+    # "SE": {
+    #     "zone": PURPLE[0],
+    #     "piece": ORANGE[1]
+    # },
+    # "S": {
+    #     "zone": GREEN[0],
+    #     "piece": RED[1]
+    # },
+    # "SW": {
+    #     "zone": "dodger_blue2",
+    #     "piece": "white"
+    # },
+    # "NW": {
+    #     "zone": ORANGE[0],
+    #     "piece": PURPLE[1]
+    # }
+
 }
 
 VALID_COORDS = {
